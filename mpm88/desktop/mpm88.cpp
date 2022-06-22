16196019c9eff39c24ac85c730235c1099f89b8d
Ailing Zhang <ailing@taichi.graphics>
2022-06-22 16:24:32 +0800

    Refactor mpm88 demo

--- mpm88/desktop/mpm88.cpp
+++ mpm88/desktop/mpm88.cpp
@@ -1,186 +1,263 @@
-#include <signal.h>
-#include <iostream>
 #include <chrono>
+#include <iostream>
+#include <signal.h>
 
-#include <taichi/runtime/program_impls/vulkan/vulkan_program.h>
+#include "mpm88.hpp"
+#include <inttypes.h>
+#include <taichi/aot/graph_data.h>
 #include <taichi/backends/vulkan/vulkan_common.h>
 #include <taichi/backends/vulkan/vulkan_loader.h>
 #include <taichi/runtime/gfx/aot_module_loader_impl.h>
-#include <taichi/aot/graph_data.h>
-#include <inttypes.h>
+#include <taichi/runtime/program_impls/vulkan/vulkan_program.h>
+#include <unistd.h>
 
-#include <taichi/gui/gui.h>
-#include <taichi/ui/backends/vulkan/renderer.h>
+namespace demo {
+namespace {
+constexpr int kNrParticles = 8192 * 2;
+constexpr int kNGrid = 128;
 
-#define NR_PARTICLES (8192*8)
-#define N_GRID 128
-void get_data(
-    taichi::lang::gfx::GfxRuntime *vulkan_runtime,
-    taichi::lang::DeviceAllocation &alloc,
-    void *data,
-    size_t size) {
+void ReadDataToHost(taichi::lang::gfx::GfxRuntime *vulkan_runtime,
+                    taichi::lang::DeviceAllocation &alloc, void *data,
+                    size_t size) {
   char *const device_arr_ptr =
       reinterpret_cast<char *>(vulkan_runtime->get_ti_device()->map(alloc));
   TI_ASSERT(device_arr_ptr);
   std::memcpy(data, device_arr_ptr, size);
   vulkan_runtime->get_ti_device()->unmap(alloc);
 }
-#include <unistd.h>
-int main() {
-    // Init gl window
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(512, 512, "Taichi show", NULL, NULL);
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+} // namespace
 
-    // Create a GGUI configuration
-    taichi::ui::AppConfig app_config;
-    app_config.name         = "MPM88";
-    app_config.width        = 512;
-    app_config.height       = 512;
-    app_config.vsync        = true;
-    app_config.show_window  = false;
-    app_config.package_path = "../"; // make it flexible later
-    app_config.ti_arch      = taichi::Arch::vulkan;
+class MPM88DemoImpl {
+public:
+  MPM88DemoImpl(taichi::lang::vulkan::VulkanDevice *device) : device_(device) {
+    InitTaichiRuntime(device_);
 
-    // Create GUI & renderer
-    auto renderer  = std::make_unique<taichi::ui::vulkan::Renderer>();
-    renderer->init(nullptr, window, app_config);
-
-    // Initialize our Vulkan Program pipeline
-    taichi::uint64 *result_buffer{nullptr};
-    auto memory_pool = std::make_unique<taichi::lang::MemoryPool>(taichi::Arch::vulkan, nullptr);
-    result_buffer = (taichi::uint64 *)memory_pool->allocate(sizeof(taichi::uint64) * taichi_result_buffer_entries, 8);
-    // Create Taichi Device for computation
-    taichi::lang::vulkan::VulkanDevice *device_ = &(renderer->app_context().device());
-    // Create Vulkan runtime
-    taichi::lang::gfx::GfxRuntime::Params params;
-    params.host_result_buffer = result_buffer;
-    params.device = device_;
-    auto vulkan_runtime =
-      std::make_unique<taichi::lang::gfx::GfxRuntime>(std::move(params));
-
-
-    // Retrieve kernels/fields/etc from AOT module so we can initialize our
-    // runtime
     taichi::lang::gfx::AotModuleParams mod_params;
     mod_params.module_path = "../shaders/";
     mod_params.runtime = vulkan_runtime.get();
-    std::unique_ptr<taichi::lang::aot::Module> module = taichi::lang::aot::Module::load(taichi::Arch::vulkan, mod_params);
+    module = taichi::lang::aot::Module::load(taichi::Arch::vulkan, mod_params);
 
     auto root_size = module->get_root_size();
-    printf("root buffer size=%ld\n", root_size);
     vulkan_runtime->add_root_buffer(root_size);
 
-    auto g_init = module->get_graph("init");
-    auto g_update = module->get_graph("update");
-
+    g_init_ = module->get_graph("init");
+    g_update_ = module->get_graph("update");
 
     // Prepare Ndarray for model
-    taichi::lang::Device::AllocParams alloc_params;
-    alloc_params.host_write = false;
-    alloc_params.host_read = false;
-    alloc_params.size = NR_PARTICLES * 2 * sizeof(float);
-    alloc_params.usage = taichi::lang::AllocUsage::Storage;
+    const std::vector<int> vec2_shape = {2};
+    const std::vector<int> vec3_shape = {3};
+    const std::vector<int> mat2_shape = {2, 2};
 
-    taichi::lang::DeviceAllocation devalloc_x = device_->allocate_memory(alloc_params);
-    auto x = taichi::lang::Ndarray(devalloc_x, taichi::lang::PrimitiveType::f32, {NR_PARTICLES}, {2});
+    x_ = NdarrayAndMem::Make(device_, taichi::lang::PrimitiveType::f32,
+                             {kNrParticles}, vec2_shape,
+                             /*host_read=*/true, /*host_write=*/true);
+    v_ = NdarrayAndMem::Make(device_, taichi::lang::PrimitiveType::f32,
+                             {kNrParticles}, vec2_shape);
+    pos_ = NdarrayAndMem::Make(device_, taichi::lang::PrimitiveType::f32,
+                               {kNrParticles}, vec3_shape);
+    C_ = NdarrayAndMem::Make(device_, taichi::lang::PrimitiveType::f32,
+                             {kNrParticles}, mat2_shape);
+    J_ = NdarrayAndMem::Make(device_, taichi::lang::PrimitiveType::f32,
+                             {kNrParticles});
 
-    // For debugging
-    //float arr[NR_PARTICLES * 2];
+    grid_v_ = NdarrayAndMem::Make(device_, taichi::lang::PrimitiveType::f32,
+                                  {kNGrid, kNGrid}, vec2_shape);
+    grid_m_ = NdarrayAndMem::Make(device_, taichi::lang::PrimitiveType::f32,
+                                  {kNGrid, kNGrid});
 
-    taichi::lang::DeviceAllocation devalloc_v = device_->allocate_memory(alloc_params);
-    auto v = taichi::lang::Ndarray(devalloc_v, taichi::lang::PrimitiveType::f32, {NR_PARTICLES}, {2});
+    args_.insert({"x", taichi::lang::aot::IValue::create(x_->ndarray())});
+    args_.insert({"v", taichi::lang::aot::IValue::create(v_->ndarray())});
+    args_.insert({"J", taichi::lang::aot::IValue::create(J_->ndarray())});
+    args_.insert({"C", taichi::lang::aot::IValue::create(C_->ndarray())});
+    args_.insert(
+        {"grid_v", taichi::lang::aot::IValue::create(grid_v_->ndarray())});
+    args_.insert(
+        {"grid_m", taichi::lang::aot::IValue::create(grid_m_->ndarray())});
+    args_.insert({"pos", taichi::lang::aot::IValue::create(pos_->ndarray())});
 
-    alloc_params.size = NR_PARTICLES * 3 * sizeof(float);
-    taichi::lang::DeviceAllocation devalloc_pos = device_->allocate_memory(alloc_params);
-    auto pos = taichi::lang::Ndarray(devalloc_pos, taichi::lang::PrimitiveType::f32, {NR_PARTICLES}, {3});
+    Reset();
+  }
 
-    alloc_params.size = NR_PARTICLES * sizeof(float) * 2 * 2;
-    taichi::lang::DeviceAllocation devalloc_C = device_->allocate_memory(alloc_params);
-    auto C = taichi::lang::Ndarray(devalloc_C, taichi::lang::PrimitiveType::f32, {NR_PARTICLES}, {2, 2});
+  ~MPM88DemoImpl() {}
 
-    alloc_params.size = NR_PARTICLES * sizeof(float);
-    taichi::lang::DeviceAllocation devalloc_J = device_->allocate_memory(alloc_params);
-    auto J = taichi::lang::Ndarray(devalloc_J, taichi::lang::PrimitiveType::f32, {NR_PARTICLES});
-
-    alloc_params.size = N_GRID * N_GRID * 2 * sizeof(float);
-    taichi::lang::DeviceAllocation devalloc_grid_v = device_->allocate_memory(alloc_params);
-    auto grid_v = taichi::lang::Ndarray(devalloc_grid_v, taichi::lang::PrimitiveType::f32, {N_GRID, N_GRID}, {2});
-
-    alloc_params.size = N_GRID * N_GRID * sizeof(float);
-    taichi::lang::DeviceAllocation devalloc_grid_m = device_->allocate_memory(alloc_params);
-    auto grid_m = taichi::lang::Ndarray(devalloc_grid_m, taichi::lang::PrimitiveType::f32, {N_GRID, N_GRID});
-
-
-    std::unordered_map<std::string, taichi::lang::aot::IValue> args;
-    args.insert({"x", taichi::lang::aot::IValue::create(x)});
-    args.insert({"v", taichi::lang::aot::IValue::create(v)});
-    args.insert({"J", taichi::lang::aot::IValue::create(J)});
-
-    g_init->run(args);
+  void Reset() {
+    g_init_->run(args_);
     vulkan_runtime->synchronize();
+  }
 
+  void Step() {
+    g_update_->run(args_);
+    vulkan_runtime->synchronize();
+  }
 
-    // Create a GUI even though it's not used in our case (required to
-    // render the renderer)
-    auto gui = std::make_shared<taichi::ui::vulkan::Gui>(&renderer->app_context(), &renderer->swap_chain(), window);
+  const taichi::lang::DeviceAllocation &pos() { return pos_->devalloc(); }
 
+private:
+  class NdarrayAndMem {
+  public:
+    NdarrayAndMem() = default;
+    ~NdarrayAndMem() { device_->dealloc_memory(devalloc_); }
 
-    // Describe information to render the circle with Vulkan
-    taichi::ui::FieldInfo f_info;
-    f_info.valid        = true;
-    f_info.field_type   = taichi::ui::FieldType::Scalar;
-    f_info.matrix_rows  = 1;
-    f_info.matrix_cols  = 1;
-    f_info.shape        = {NR_PARTICLES};
-    f_info.field_source = taichi::ui::FieldSource::TaichiVulkan;
-    f_info.dtype        = taichi::lang::PrimitiveType::f32;
-    f_info.snode        = nullptr;
-    f_info.dev_alloc    = devalloc_pos;
-    taichi::ui::CirclesInfo circles;
-    circles.renderable_info.has_per_vertex_color = false;
-    circles.renderable_info.vbo_attrs = taichi::ui::VertexAttributes::kPos;
-    circles.renderable_info.vbo                  = f_info;
-    circles.color                                = {0.8, 0.4, 0.1};
-    circles.radius                               = 0.005f; // 0.0015f looks unclear on desktop
+    const taichi::lang::Ndarray &ndarray() const { return *ndarray_; }
 
-    renderer->set_background_color({0.6, 0.6, 0.6});
+    taichi::lang::DeviceAllocation &devalloc() { return devalloc_; }
 
-    args.insert({"C", taichi::lang::aot::IValue::create(C)});
-    args.insert({"grid_v", taichi::lang::aot::IValue::create(grid_v)});
-    args.insert({"grid_m", taichi::lang::aot::IValue::create(grid_m)});
-    args.insert({"pos", taichi::lang::aot::IValue::create(pos)});
-
-    int count = 0;
-    while (!glfwWindowShouldClose(window)) {
-        g_update->run(args);
-        vulkan_runtime->synchronize();
-
-        // Render elements
-        renderer->circles(circles);
-        renderer->draw_frame(gui.get());
-        renderer->swap_chain().surface().present_image();
-        renderer->prepare_for_next_frame();
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    static std::unique_ptr<NdarrayAndMem>
+    Make(taichi::lang::Device *device, taichi::lang::DataType dtype,
+         const std::vector<int> &arr_shape,
+         const std::vector<int> &element_shape = {}, bool host_read = false,
+         bool host_write = false) {
+      // TODO: Cannot use data_type_size() until
+      // https://github.com/taichi-dev/taichi/pull/5220.
+      // uint64_t alloc_size = taichi::lang::data_type_size(dtype);
+      uint64_t alloc_size = 1;
+      if (auto *prim = dtype->as<taichi::lang::PrimitiveType>()) {
+        using PT = taichi::lang::PrimitiveType;
+        if (prim == PT::f32 || prim == PT::i32 || prim == PT::u32) {
+          alloc_size = 4;
+        } else {
+          TI_ERROR("Unsupported bit width!");
+          return nullptr;
+        }
+      } else {
+        TI_ERROR("Non primitive type!");
+        return nullptr;
+      }
+      for (int s : arr_shape) {
+        alloc_size *= s;
+      }
+      for (int s : element_shape) {
+        alloc_size *= s;
+      }
+      taichi::lang::Device::AllocParams alloc_params;
+      alloc_params.host_read = host_read;
+      alloc_params.host_write = host_write;
+      alloc_params.size = alloc_size;
+      alloc_params.usage = taichi::lang::AllocUsage::Storage;
+      auto res = std::make_unique<NdarrayAndMem>();
+      res->device_ = device;
+      res->devalloc_ = device->allocate_memory(alloc_params);
+      res->ndarray_ = std::make_unique<taichi::lang::Ndarray>(
+          res->devalloc_, dtype, arr_shape, element_shape);
+      return res;
     }
 
-    device_->dealloc_memory(devalloc_x);
-    device_->dealloc_memory(devalloc_v);
-    device_->dealloc_memory(devalloc_J);
-    device_->dealloc_memory(devalloc_C);
-    device_->dealloc_memory(devalloc_grid_v);
-    device_->dealloc_memory(devalloc_grid_m);
-    device_->dealloc_memory(devalloc_pos);
+  private:
+    taichi::lang::Device *device_{nullptr};
+    std::unique_ptr<taichi::lang::Ndarray> ndarray_{nullptr};
+    taichi::lang::DeviceAllocation devalloc_;
+  };
 
-    vulkan_runtime.reset();
-    renderer->cleanup();
+  void InitTaichiRuntime(taichi::lang::vulkan::VulkanDevice *device_) {
+    // Create Vulkan runtime
+    taichi::lang::gfx::GfxRuntime::Params params;
+    result_buffer_.resize(taichi_result_buffer_entries);
+    params.host_result_buffer = result_buffer_.data();
+    params.device = device_;
+    vulkan_runtime =
+        std::make_unique<taichi::lang::gfx::GfxRuntime>(std::move(params));
+  }
 
-    return 0;
+  std::unique_ptr<taichi::lang::vulkan::VulkanDeviceCreator> embedded_device_{
+      nullptr};
+  taichi::lang::vulkan::VulkanDevice *device_{nullptr};
+  std::vector<uint64_t> result_buffer_;
+  std::unique_ptr<taichi::lang::gfx::GfxRuntime> vulkan_runtime{nullptr};
+
+  std::unique_ptr<taichi::lang::aot::Module> module{nullptr};
+  std::unique_ptr<NdarrayAndMem> x_{nullptr};
+  std::unique_ptr<NdarrayAndMem> v_{nullptr};
+  std::unique_ptr<NdarrayAndMem> J_{nullptr};
+  std::unique_ptr<NdarrayAndMem> C_{nullptr};
+  std::unique_ptr<NdarrayAndMem> grid_v_{nullptr};
+  std::unique_ptr<NdarrayAndMem> grid_m_{nullptr};
+  std::unique_ptr<NdarrayAndMem> pos_{nullptr};
+  std::unique_ptr<taichi::lang::aot::CompiledGraph> g_init_{nullptr};
+  std::unique_ptr<taichi::lang::aot::CompiledGraph> g_update_{nullptr};
+
+  std::unordered_map<std::string, taichi::lang::aot::IValue> args_;
+};
+
+MPM88Demo::MPM88Demo() {
+  // Init gl window
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  window = glfwCreateWindow(512, 512, "Taichi show", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+  }
+
+  // Create a GGUI configuration
+  taichi::ui::AppConfig app_config;
+  app_config.name = "MPM88";
+  app_config.width = 512;
+  app_config.height = 512;
+  app_config.vsync = true;
+  app_config.show_window = false;
+  app_config.package_path = "../"; // make it flexible later
+  app_config.ti_arch = taichi::Arch::vulkan;
+
+  // Create GUI & renderer
+  renderer = std::make_unique<taichi::ui::vulkan::Renderer>();
+  renderer->init(nullptr, window, app_config);
+
+  renderer->set_background_color({0.6, 0.6, 0.6});
+
+  gui_ = std::make_shared<taichi::ui::vulkan::Gui>(
+      &renderer->app_context(), &renderer->swap_chain(), window);
+
+  // Create Taichi Device for computation
+  taichi::lang::vulkan::VulkanDevice *device_ =
+      &(renderer->app_context().device());
+
+  impl_ = std::make_unique<MPM88DemoImpl>(device_);
+
+  // Describe information to render the circle with Vulkan
+  f_info.valid = true;
+  f_info.field_type = taichi::ui::FieldType::Scalar;
+  f_info.matrix_rows = 1;
+  f_info.matrix_cols = 1;
+  f_info.shape = {kNrParticles};
+  f_info.field_source = taichi::ui::FieldSource::TaichiVulkan;
+  f_info.dtype = taichi::lang::PrimitiveType::f32;
+  f_info.snode = nullptr;
+  f_info.dev_alloc = impl_->pos();
+
+  circles.renderable_info.has_per_vertex_color = false;
+  circles.renderable_info.vbo_attrs = taichi::ui::VertexAttributes::kPos;
+  circles.renderable_info.vbo = f_info;
+  circles.color = {0.8, 0.4, 0.1};
+  circles.radius = 0.005f; // 0.0015f looks unclear on desktop
+}
+
+void MPM88Demo::Step() {
+  while (!glfwWindowShouldClose(window)) {
+    impl_->Step();
+
+    // Render elements
+    renderer->circles(circles);
+    renderer->draw_frame(gui_.get());
+    renderer->swap_chain().surface().present_image();
+    renderer->prepare_for_next_frame();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+}
+
+MPM88Demo::~MPM88Demo() {
+  impl_.reset();
+  gui_.reset();
+  // renderer owns the device so it must be destructed last.
+  renderer.reset();
+}
+
+} // namespace demo
+
+int main() {
+  auto mpm88_demo = std::make_unique<demo::MPM88Demo>();
+  mpm88_demo->Step();
+
+  return 0;
 }
